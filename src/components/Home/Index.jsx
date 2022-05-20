92c5b40023b6e6261d0a203f23c20f7647f46682
fjmg84 <fjmgqba@gmail.com>
2022-05-20 11:22:47 -0400

    add component Spinner

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -1,42 +1,45 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getFriends } from "../../redux/actions/friends";
-import { setAlert } from "../../redux/actions/alerts";
-import { TYPES } from "../../redux/types";
-import { List } from "./List";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getFriends } from '../../redux/actions/friends'
+import { setAlert } from '../../redux/actions/alerts'
+import { TYPES } from '../../redux/types'
+import { List } from './List'
+import { Spinner } from '../Helps/Spinner'
 
 export const Index = () => {
-  const dispatch = useDispatch();
-  const { status, msg, classAlert } = useSelector(
-    (state) => state.alertsReducer
-  );
-  const friends = useSelector((state) => state.friendsReducer);
+	const dispatch = useDispatch()
+	const { status, msg, classAlert } = useSelector(
+		(state) => state.alertsReducer,
+	)
+	const friends = useSelector((state) => state.friendsReducer)
 
-  useEffect(() => {
-    dispatch(setAlert(true, "Loading friends...", "loading"));
-    getFriends()
-      .then((friends) => {
-        dispatch({ type: TYPES.LOAD, payload: friends });
-        dispatch(setAlert(false, undefined));
-      })
-      .catch((err) => {
-        console.log(err);
-        dispatch(
-          setAlert(true, "Up!!! Sorry Not Found List Of Friends :(", "error")
-        );
-      });
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(setAlert(true, 'Loading friends...', 'loading'))
+		getFriends()
+			.then((friends) => {
+				dispatch({ type: TYPES.LOAD, payload: friends })
+				dispatch(setAlert(false, undefined))
+			})
+			.catch((err) => {
+				console.log(err)
+				dispatch(
+					setAlert(true, 'Up!!! Sorry Not Found List Of Friends :(', 'error'),
+				)
+			})
+	}, [dispatch])
 
-  return (
-    <div className="box">
-      <div className="body">
-        <div className="header">
-          <h1>Friends</h1>
-          {status && <div className={classAlert}>{msg}</div>}
-        </div>
+	return (
+		<div className="box">
+			<div className="body">
+				<div className="header">
+					<h1>Friends</h1>
+					{status && <div className={classAlert}>{msg}</div>}
+				</div>
 
-        <div className="main">{friends && <List friends={friends} />}</div>
-      </div>
-    </div>
-  );
-};
+				<div className="main">
+					{friends.length > 0 ? <List friends={friends} /> : <Spinner />}
+				</div>
+			</div>
+		</div>
+	)
+}
